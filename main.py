791cc9d310098f86adf9deb38e3799b14b1a72be
Nikoay <nenre@yandex.ru>
2023-06-09 16:42:29 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def run_mark():
+    # Переходим на страницу авторизации
+    driver.get('http://petfriends.skillfactory.ru/login')
+    # Вводим email
+    driver.find_element(By.ID, 'email').send_keys('nenre@yandex.ru')
+    # Вводим пароль
+    driver.find_element(By.ID, 'pass').send_keys('127562')
+    # Нажимаем на кнопку входа в аккаунт
+    driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    driver.find_element(By.CSS_SELECTOR, 'a[href="/my_pets"]').click()
+
+    yield
+
+    driver.quit()
